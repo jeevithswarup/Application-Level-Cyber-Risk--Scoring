ee934395d086c44138c8b5e15ef86ca4b26820d5
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-02-10 10:41:25 +0530

    created userprofile serializer.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,2 +1,19 @@
 from rest_framework import serializers
 from .models import UserProfile
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=UserProfile
+        fields=[
+            'id',
+            'username',
+            'email',
+            'role',
+            'risk_score',
+            'account_status',
+            'last_ip',
+            'last_device',
+        ]
+        read_only_fields = ['risk_score', 'last_ip', 'last_device']
