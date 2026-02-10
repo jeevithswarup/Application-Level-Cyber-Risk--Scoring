70ff1573d5cfdb67eaaeab3af5f9d33d7d8a5aea
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-02-10 11:10:08 +0530

    Created a logic for registering the user  in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -5,5 +5,20 @@ from rest_framework.decorators import api_view
 from .models import UserProfile
 
 
+@api_view(['POST'])
+def Register(request):
+    username=request.data.get('username')
+    password=request.data.get('password')
+    email=request.data.get('email')
+
+    if UserProfile.objects.filter(username=username).exists():
+        return Response({'error':'Username already exists'},status=400)
+    
+    user=UserProfile.objects.create_user(
+        username=username,
+        password=password,
+        email=email,
+    )
+    return Response({'message':'User is sucessfully Created'})
 
 
