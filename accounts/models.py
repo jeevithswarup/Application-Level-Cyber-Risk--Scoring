b23640a222e2c7162a4fb6fb2d037b2f423ce3a5
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-02-10 10:08:12 +0530

    Setup of environment

---
+++ accounts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class UserProfile(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'User'),
+        ('admin', 'Admin'),
+    )
+    STATUS_CHOICES = (
+        ('active', 'Active'),
+        ('restricted', 'Restricted'),
+        ('blocked', 'Blocked'),
+    )
+
+    role=models.CharField(max_length=10,choices=ROLE_CHOICES,default='user')
+    risk_score=models.IntegerField(default=0)
+    last_ip = models.GenericIPAddressField(null=True,blank=True)
+    last_device=models.TextField(null=True,blank=True)
+
+        
