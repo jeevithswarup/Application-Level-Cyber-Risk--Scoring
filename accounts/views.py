9ca836b7b21aa7da99d5022dd9d8fc9cc81ea10a
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-02-10 11:25:51 +0530

    creaed the login logic  in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -19,6 +19,30 @@ def Register(request):
         password=password,
         email=email,
     )
+
     return Response({'message':'User is sucessfully Created'})
+
+
+@api_view(['POST'])
+def login_view(request):
+    username=request.data.get('username')
+    password=request.data.get('password')
+     
+    user=authenticate(username=username,password=password)
+
+    if user is None:
+        return Response({'error':'Invalid Credentials'},status=401)
+    
+    if user.account_status =='blocked':
+        return Response({'error':'Account is Blocked'},status=403)
+    
+    user.last_ip=request.META.get('REMOTE ADDRESS')
+    user.last_device=request.META.get('HTTP_USER_AGENT')
+    user.save()
+
+    login(request,user)
+
+
+    return Response({'message':'Login Successful'})
 
 
