7776fd0b392195a1ee762c2c67c2595b7c08611d
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-02-13 17:10:10 +0530

    added dashboar.html'

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
      path('',Register,name="Register"),
      path('login/',login_view,name='login'),
+     path('dashboard/',dashboard,name="dashboard"),
     
 ]
